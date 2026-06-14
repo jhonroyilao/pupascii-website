cd4e49c1b832c8609818113892102fc1437ba2d1
jhonroyilao <jhonroyilao@gmail.com>
2026-06-15 01:21:42 +0800

    refactor(layout): add smoot cursor

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,6 +4,7 @@ import {
   Inter,
   Bricolage_Grotesque,
 } from "next/font/google";
+import { SmoothCursor } from "@/components/ui/smooth-cursor"
 
 const instrumentSans = Instrument_Sans({
   subsets: ["latin"],
@@ -41,6 +42,7 @@ export default function RootLayout({ children }) {
         className={`${instrumentSans.variable} ${inter.variable} ${bricolage.variable}`}
       >
         {children}
+        <SmoothCursor />
       </body>
     </html>
   );
